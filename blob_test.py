4cff90551fcbd82dbee280084e35349279143f3b
stephaniiema <54160019+stephaniiema@users.noreply.github.com>
2022-04-21 12:19:42 -0500

    unsplash from text files

--- blob_test.py
+++ blob_test.py
@@ -9,15 +9,18 @@ access_key = os.getenv('ACCESS_KEY')
 image_api = 'https://api.unsplash.com/'
 
 # The text to analyze
-filename = 'data/chamounix_synopsis.txt'
+# filename = 'data/chamounix_synopsis.txt'
+filename = 'data/hmspinafore_synopsis.txt'
 file = open(filename, 'r')
 text = file.read()
+text_list= text.split('.')
 
 # blob = TextBlob(text)
 # print(blob.tags)
-
-payload = {'client_id': access_key, 'query': 'clouds', 'page': '1', 'per_page': '1'}
-r = requests.get(image_api+'search/photos', params=payload)
-resp = r.json()
-# print(json.dumps(resp, indent=1))
-print(resp['results'][0]['urls']['raw'])
+for s in text_list:
+    payload = {'client_id': access_key, 'query': s, 'page': '1', 'per_page': '1'}
+    r = requests.get(image_api+'search/photos', params=payload)
+    resp = r.json()
+    # print(json.dumps(resp, indent=1))
+    print("\n\n"+s)
+    print(resp['results'][0]['urls']['raw'])
